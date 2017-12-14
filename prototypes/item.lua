c508040e67dc2919bfe44e25bf18d0f32c0e0b12
cappie <cappie@users.noreply.github.com>
2017-12-14 19:08:49 +0100

    Update item.lua
    - Added missing variable icon_size

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -8,6 +8,7 @@ data:extend({
     flow_color = {r=0, g=0, b=0},
     max_temperature = 100,
     icon = "__Wood_Gasification__/graphics/tar.png",
+    icon_size = 32,
     pressure_to_speed_ratio = 0.4,
     flow_to_energy_ratio = 0.59,
     order = "a[fluid]-t[tar]"
